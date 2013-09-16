8c64baa0dd144d2c60d0b432f074d5b8167dee7e
antonydrew <adrew@luc.edu>
2013-09-16 14:23:07 -0500

    Second commit
    *Inlcudes Class Diagram for Domain Model (in ASTAH)
    *Includes JAVA code for classes - still needs functions/get/set/override

---
+++ Project1-442/AcademicInfo.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class AcademicInfo {
+
+	private String ProgramName;
+
+	private String CompCourses;
+
+	private int CompUnits;
+
+	private Date GradDate;
+
+	private String EnrolledCourses;
+
+	private int EnrolledUnits;
+
+	private double GPA;
+
+	private boolean Hold;
+
+	public String GetClassSchedule() {
+		return null;
+	}
+
+	public int GetNumUnits() {
+		return 0;
+	}
+
+	public double GetGrades() {
+		return 0;
+	}
+
+	public int AddCourse() {
+		return 0;
+	}
+
+	public int DropCourse() {
+		return 0;
+	}
+
+	public boolean CheckRequirements() {
+		return false;
+	}
+
+}
